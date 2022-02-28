21a9bc443adc7576508c0b82312e87210a84cbd7
QuatreCC <100638363+QuatreCC@users.noreply.github.com>
2022-03-01 02:39:27 +0300

    Quatreblockchain

---
+++ src/config/Ascii.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2020, The Ionize Developers
+// Copyright (c) 2018, The DeroGold Association
+// Copyright (c) 2020, The Ionize Developers
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt = 
+" ______    __  __   ________   _________  ______    ______      "
+"/_____/\  /_/\/_/\ /_______/\ /________/\/_____/\  /_____/\     "
+"\:::_ \ \ \:\ \:\ \\::: _  \ \\__.::.__\/\:::_ \ \ \::::_\/_    "
+" \:\ \ \ \_\:\ \:\ \\::(_)  \ \  \::\ \   \:(_) ) )_\:\/___/\   "
+"  \:\ \ /_ \\:\ \:\ \\:: __  \ \  \::\ \   \: __ `\ \\::___\/_  "
+"   \:\_-  \ \\:\_\:\ \\:.\ \  \ \  \::\ \   \ \ `\ \ \\:\____/\ "
+"    \___|\_\_/\_____\/ \__\/\__\/   \__\/    \_\/ \_\/ \_____\/ "
+"                                                                ";
